93391d2bf0b9f51702721cb6a057763b32c578ce
mythosmystery <axiysfire@gmail.com>
2021-06-04 16:26:55 -0500

    updated api routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,14 @@
-//const { User, Post } = require('../../models');
-//const withAuth = require("../../utils/auth");
+const { User, Post } = require('../../models');
+const withAuth = require('../../utils/auth');
 const router = require('express').Router();
-router.get('/', (req, res) => {
-    res.send('<h1>post api route</h1>')
-})
+router.get('/', async (req, res) => {
+   try {
+      const posts = await Post.findAll({
+         include: [{ model: User }],
+      });
+      res.status(200).json(posts);
+   } catch (err) {
+      res.status(400).json(err);
+   }
+});
 module.exports = router;
