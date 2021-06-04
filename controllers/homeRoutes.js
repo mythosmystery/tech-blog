f3b7fc058000acc51b4b27fd55caf1a54d815316
mythosmystery <axiysfire@gmail.com>
2021-06-04 16:17:00 -0500

    added user model

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,9 @@
 //const withAuth = require("../../utils/auth");
 const router = require('express').Router();
 router.get('/', (req, res) => {
-    res.render('homepage');
+   res.render('homepage', {
+      logged_in: true,
+      posts: [{ title: 'Post 1', body: 'Test post', user: { name: 'Test User' } }],
+   });
 });
 module.exports = router;
